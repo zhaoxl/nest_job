d7ef23c4c91ca98606cb81ff568aab4daaeec188
99866770_m <99866770@qq.com>
2014-06-04 11:40:30 +0800

    修改路由错误

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ NestJob::Application.routes.draw do
   get '/' => 'application#index'
 
 
-  resource :posts
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
