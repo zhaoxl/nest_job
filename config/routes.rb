638a4ad4ed709df4245cc4e86fc7943d8c0ddc14
99866770_m <99866770@qq.com>
2014-06-04 11:32:18 +0800

    Signed-off-by: 99866770_m <99866770@qq.com>

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ NestJob::Application.routes.draw do
   end
     
   get '/' => 'application#index'
+
+  resource :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
